85fca4ad5d1c183f201bed1427947488b99fdb53
Tom Verbeek <58432711+Johnicusswagicus@users.noreply.github.com>
2021-11-02 14:06:51 +0100

    meer coole jazz

--- src/Pentris.java
+++ src/Pentris.java
@@ -151,7 +151,46 @@ public class Pentris {
         }
     }
 
-
+    //this function evaluated if a piece can be placed on a give grid at a certain x and y location
+    public static Boolean PieceFit(int[][]grid,int PieceID,int Piecemutation,int x,int y){
+        //if the x is negative then the starting point is of the grid and therefor invalid
+        if(x<0){
+                    
+                return false;
+            }
+    
+            //shorthand for readability
+            int[][][][]database=PentominoDatabase.data;
+    
+    
+            //if the piece doesnt extend past the borders
+            if(grid.length>y+database[PieceID][Piecemutation].length-1){
+                
+                
+                if(grid[0].length>x+database[PieceID][Piecemutation][0].length-1){
+                    
+    
+    
+                    //then it wil check for every square whether the matrix has a 1 there(meaning its a square to be placed)
+                    //and if the grid already has a value there
+                    for(int i=0;i<database[PieceID][Piecemutation].length;i++){
+                        for(int j=0;j<database[PieceID][Piecemutation][0].length;j++){
+                            if(grid[i+y][j+x]>=0&database[PieceID][Piecemutation][i][j]==1){
+                                ///if so they overlap so you cant place the piece
+                                return false;
+                            }
+                        }
+                    }
+                    //int[][]gridclone=clone2Dint(grid);
+                    //Search.addPiece(gridclone, database[PieceID][Piecemutation], Piecemutation, y, x);
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
 
 // here should write the methods which are used when you start a game
     public Pentris(){

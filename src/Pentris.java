206443cc97e1802dc4eba06952bdb2ab2549743c
Blackbirb <Liannevdnbroek@gmail.com>
2021-11-10 13:18:58 +0100

    nextPiece, still needs to be checked

--- src/Pentris.java
+++ src/Pentris.java
@@ -70,54 +70,52 @@ public class Pentris {
         }
     }
 
-    private static ArrayList<Integer> nextPieces = new ArrayList<Integer>();
+    // private static ArrayList<Integer> nextPieces = new ArrayList<Integer>();
 
-    public static void nextPiece() {
-        PieceX = StartX;
-        PieceY = StartY;
-        rotation = 0;
-        if (nextPieces.isEmpty()) {
-            Collections.addAll(nextPieces, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
-            Collections.shuffle(nextPieces);
-        }
+    // public static void nextPiece() {
+    // PieceX = StartX;
+    // PieceY = StartY;
+    // rotation = 0;
+    // if (nextPieces.isEmpty()) {
+    // Collections.addAll(nextPieces, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+    // Collections.shuffle(nextPieces);
+    // }
 
-        // System.out.println(nextPieces.get(0));
-        pieceID = nextPieces.get(0);
-        nextPieces.remove(0);
-    }
+    // // System.out.println(nextPieces.get(0));
+    // pieceID = nextPieces.get(0);
+    // nextPieces.remove(0);
+    // }
+
     // this method should update the nextpiece and pieceid variables
     // every piece should get its turn in 12 pieces
-    // public static void nextPiece() { // Lianne
-    // if (pentPieces.size() < 1) {
-    // // If there is only one element in the arraylist, clear the arraylist
-    // // and add all the IDs to the arraylist again
-    // pieceIDs.clear(); // remove all IDs from the pieceIDs array
-    // pentPieces.add(0);
-    // pentPieces.add(1);
-    // pentPieces.add(2);
-    // pentPieces.add(3);
-    // pentPieces.add(4);
-    // pentPieces.add(5);
-    // pentPieces.add(6);
-    // pentPieces.add(7);
-    // pentPieces.add(8);
-    // pentPieces.add(9);
-    // pentPieces.add(10);
-    // pentPieces.add(11); // make an arraylist with the pentomino IDs
-    // } // If there's more than one element in the arraylist, you can get a
-    // pentomino
-    // // out of the list
-    // Collections.shuffle(pentPieces); // randomize the order of the arraylist
-    // for (int i = 0; i < pentPieces.size(); i++) { // loop through shuffled
-    // arraylist
-    // pieceIDs.add(pentPieces.get(i)); // take the first ID and add it to the
-    // pieceIDs arraylist
-    // pentPieces.remove(i); // remove that piece from the pentPieces arraylist
-    // }
-    // PieceX = StartX; // reset starting points
-    // PieceY = StartY;
-    // rotation = 0; // reset rotation to 0
-    // }
+    public static void nextPiece() { // Lianne
+        PieceX = StartX; // reset starting points
+        PieceY = StartY;
+        rotation = 0; // reset rotation to 0
+        if (pentPieces.size() < 1) {
+            // If there is only one element in the arraylist, clear the arraylist
+            // and add all the IDs to the arraylist again
+            pieceIDs.clear(); // remove all IDs from the pieceIDs array
+            pentPieces.add(0);
+            pentPieces.add(1);
+            pentPieces.add(2);
+            pentPieces.add(3);
+            pentPieces.add(4);
+            pentPieces.add(5);
+            pentPieces.add(6);
+            pentPieces.add(7);
+            pentPieces.add(8);
+            pentPieces.add(9);
+            pentPieces.add(10);
+            pentPieces.add(11); // make an arraylist with the pentomino IDs
+            Collections.shuffle(pentPieces); // randomize the order of the arraylist
+        } // If there's more than one element in the arraylist, you can get a
+          // pentomino out of the list
+        pieceIDs.add(pentPieces.get(0)); // take the first ID and add it to the
+        // pieceIDs arraylist
+        pentPieces.remove(0); // remove that piece from the pentPieces arraylist
+
+    }
 
     // this method should rotate a piece if posible has to rotate left and right
     // this should be done in the rotation variable

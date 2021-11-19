a69e2642f7dea1297560cccb5a9b63f6a3615b78
Tom Verbeek <58432711+Johnicusswagicus@users.noreply.github.com>
2021-11-20 00:44:00 +0100

    ai

--- src/PentrisAI.java
+++ src/PentrisAI.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class PentrisAI {
 
     int bestrotation=0;
@@ -10,23 +12,31 @@ public class PentrisAI {
         Thread ai= new Thread() {
             public void run() {
             while(true){
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 best=bestRotationPosition(Pentris.getGrid(), Pentris.getPieceID());
                 bestrotation=best[1];
                 bestX=best[0];
-                while(Pentris.getRotation()!=bestrotation){
+                //System.out.println("x:"+bestX+"  rotation:"+bestrotation);
+                if(Pentris.getRotation()!=bestrotation){
                     Pentris.rotatePiece(true);
                 }
-                while(Pentris.getX()!=bestX){
-                    if(Pentris.getX()<bestX){
-                        Pentris.PieceX+=1;
+                
+                if(Pentris.getX()<bestX){
+                    Pentris.moveHorizontal(true);
 
-                    }
-                    if(Pentris.getX()>bestX){
-                        Pentris.PieceX+=-1;
-                        
-                    }
                 }
-                Pentris.dropPiece();
+                if(Pentris.getX()>bestX){
+                    Pentris.moveHorizontal(false);
+                    
+                    
+                }
+                if(Pentris.getX()==bestX&&Pentris.getRotation()==bestrotation){
+                    	Pentris.dropPiece();
+                }
 
 
             }
@@ -83,8 +93,10 @@ public class PentrisAI {
         //drop the piece on the cloned grid
         for (int i = 1; i < 50; i++) {
             // System.out.println("pieceY = " + PieceY);
-            if (!PieceFit(cloneGrid, pieceID, rotation, PieceY + i, PieceX)) {
+            if (!PieceFit(cloneGrid, pieceID, rotation, PieceX, PieceY+i)) {
                 PieceY += i - 1; // Piece has to be added on this Y position
+                //System.out.println("dropped  "+PieceY);
+                break;
                 
             }
         }
@@ -98,35 +110,28 @@ public class PentrisAI {
             }
         }
         //code to check amount of lines cleared
-        int linesCleared=0;
+        ArrayList<Integer> lineCords= new ArrayList<Integer>();
         boolean full=true;
         for(int i=0;i<cloneGrid.length;i++){
             for(int j=0;j<cloneGrid[i].length;j++){
                 if(cloneGrid[i][j]==-1){
                     full=false;
                 }
-
             }
             if(full){
-                for(int j=0;j<cloneGrid[i].length;j++){
-                    cloneGrid[i][j]=-1;}
-                if(i!=0){
-                    for(int j=i;j>0;j--){
-                        for(int k=0;k>cloneGrid[j].length;k++){
-                            cloneGrid[j][k]=cloneGrid[j-1][k];
-
-                        }
-                    }
-                }
-                for(int j=0;j<cloneGrid[i].length;j++){
-                    cloneGrid[0][j]=-1;}
-
-
-                linesCleared++;
-                full=false;
-                
+                lineCords.add(i);
             }
         }
+
+
+
+        int linesCleared=lineCords.size();
+        //code to remove all lines from grid
+        for(int i=0;i<lineCords.size();i++){
+            removeLine(lineCords.get(i), cloneGrid);
+        }
+
+
 
 
        
@@ -141,10 +146,10 @@ public class PentrisAI {
                 if(columnTop&&cloneGrid[j][i]==-1){
                     holes++;
                 }
-
             }
             columnTop=false;
         }
+        System.out.println(holes);
 
         int[] bla={height-linesCleared,holes};
         return bla;
@@ -153,8 +158,34 @@ public class PentrisAI {
     }
 
 
-    public static int[] bestRotationPosition(int[][]grid,int pieceID){
+       // this method removes a line from the grid
+       public static void removeLine(int line,int[][] grid) {
+        int[][] updatedGrid = new int[grid.length][grid[0].length];
+        int placeInGrid;
 
+        for (int i = 0; i < updatedGrid.length; i++) {
+            placeInGrid = grid[0].length - 1;
+            for (int gridLine = updatedGrid[0].length - 1; gridLine >= 0; gridLine--) {
+                if (placeInGrid < 0) {
+                    updatedGrid[i][gridLine] = -1;
+                } else if (gridLine == line) {
+                    placeInGrid--;
+                    updatedGrid[i][gridLine] = grid[i][placeInGrid];
+                    placeInGrid--;
+                } else {
+                    updatedGrid[i][gridLine] = grid[i][placeInGrid];
+                    placeInGrid--;
+                }
+
+            }
+        }
+        grid = updatedGrid;
+    }
+
+    public static int[] bestRotationPosition(int[][]grid1,int pieceID){
+
+
+        int[][]grid=Pentris.clone2Dint(grid1);
         int bestRotation=0;
         int bestX=0;
         int bestholes=9999;
@@ -165,8 +196,11 @@ public class PentrisAI {
                 heightAndHoles[i]=simulateDrop(grid, pieceID, j, i, 0);
 
             }
+        }
 
             
+        for(int i=0;i<heightAndHoles.length;i++){
+                //System.out.println("height:"+heightAndHoles[i][0]+"  holes:"+heightAndHoles[i][1]);
         }
 
         for(int i=0;i<heightAndHoles.length;i++){
@@ -180,6 +214,7 @@ public class PentrisAI {
 
         }
         int[] best={bestX,bestRotation};
+        
 
         
         return best;

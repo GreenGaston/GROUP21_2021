47c2615cacc127c4fd9ec2cefada8e4a4307d11d
JGeraats <j.geraats@student.maastrichtuniversity.nl>
2021-12-07 16:03:54 +0100

    Added a comment

--- src/Pentris.java
+++ src/Pentris.java
@@ -132,6 +132,7 @@ public class Pentris {
         // <Order Here>
         //------------------------------------------
         // Comment only one option in to use.
+        // MAKE SURE TO COMMENT THE SAME SCENARIO IN FOR BOTH EMPTYCHECKS AND REFILLS!!
 
 
         PieceX = StartX;
@@ -140,11 +141,11 @@ public class Pentris {
         if (nextPieces.isEmpty()) {
     //-----------------------------------------------------
     // Scenario Normal:
-            Collections.addAll(nextPieces, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
-            Collections.shuffle(nextPieces);
+            // Collections.addAll(nextPieces, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+            // Collections.shuffle(nextPieces);
     //-----------------------------------------------------
     // Scenario 1:
-            // Collections.addAll(nextPieces, 9, 4, 0, 11, 8, 3, 7, 10, 1, 6, 2, 5);
+            Collections.addAll(nextPieces, 9, 4, 0, 11, 8, 3, 7, 10, 1, 6, 2, 5);
     //-----------------------------------------------------
     // Scenario 2:
             // Collections.addAll(nextPieces, 9, 4, 0, 11, 8, 3, 10, 7, 6, 2, 5, 1);
@@ -186,11 +187,46 @@ public class Pentris {
         // System.out.println(nextPieces.get(0));
         pieceID = nextPieces.get(0);
         nextPieces.remove(0);
+
+
         // recheck for the nextpiece
-        // if (nextPieces.isEmpty()) {
-        //     Collections.addAll(nextPieces, 0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1);
-        //     Collections.shuffle(nextPieces);
-        // }
+        if (nextPieces.isEmpty()) {
+    //-----------------------------------------------------
+    // Scenario Normal:
+            // Collections.addAll(nextPieces, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
+            // Collections.shuffle(nextPieces);
+    //-----------------------------------------------------
+    // Scenario 1:
+    Collections.addAll(nextPieces, 9, 4, 0, 11, 8, 3, 7, 10, 1, 6, 2, 5);
+    //-----------------------------------------------------
+    // Scenario 2:
+            // Collections.addAll(nextPieces, 9, 4, 0, 11, 8, 3, 10, 7, 6, 2, 5, 1);
+    //-----------------------------------------------------
+    // Scenario 3:
+            // Collections.addAll(nextPieces, 1, 5, 2, 6, 7, 10, 3, 4, 0, 9, 11, 8);
+    //-----------------------------------------------------
+    // Scenario 4:
+            // Collections.addAll(nextPieces, 5, 2, 6, 7, 1, 10, 3, 8, 11, 9, 0, 4);
+    //-----------------------------------------------------
+    // Scenario 5:
+            // Collections.addAll(nextPieces, 6, 5, 7, 3, 11, 9, 2, 0, 4, 10, 8, 1);
+    //-----------------------------------------------------
+    // Scenario 6:
+            // Collections.addAll(nextPieces, 1, 8, 10, 4, 0, 2, 9, 11, 3, 7, 6, 5);
+    //-----------------------------------------------------
+    // Scenario 7:
+            // Collections.addAll(nextPieces, 8, 4, 0, 9, 10, 2, 11, 3, 7, 6, 5, 1);
+    //-----------------------------------------------------
+    // Scenario 8:
+            // Collections.addAll(nextPieces, 1, 8, 10, 4, 0, 2, 11, 7, 9, 3, 6, 5);
+    //-----------------------------------------------------
+    // Scenario 9:
+            // Collections.addAll(nextPieces, 4, 0, 8, 1, 5, 9, 6, 7, 3, 10, 11, 2);
+    //-----------------------------------------------------
+    // Scenario 10:
+            // Collections.addAll(nextPieces, 9, 11, 4, 0, 5, 8, 6, 2, 10, 7, 3, 1);
+    //-----------------------------------------------------
+        }
 
         // System.out.println(nextPieces.get(0)+"testerino");
         ui.setNextPiece(pentominoDatabase[nextPieces.get(0)][0], nextPieces.get(0));

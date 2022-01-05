1d467d1061cfdb6ec5840e732ab9bd7b7b394716
Blackbirb <60325691+LiannevdB@users.noreply.github.com>
2022-01-05 20:24:53 +0100

    Formatting

--- src/AlgorithmX.java
+++ src/AlgorithmX.java
@@ -4,19 +4,14 @@ import java.util.*;
 import java.io.*;
 
 public class AlgorithmX {
-    static boolean hasR = false;
-    static int start;
-    static int i, j, k;
-    static int rows, columns;
-    static int r, c;
-    static int countCols = 0;
-    private static ArrayList perfectSolution = new ArrayList();
+    static int i, j, k, r, c, rows, columns;
+    // private static ArrayList perfectSolution = new ArrayList();
     private static ArrayList partialSolution = new ArrayList();
 
     /*******************************************************************************************************************************************
      * DOCUMENTATION: KNUTH'S ALGORITHM X, EXACT COVER PROBLEM
      * -------------------------------------------------------------------------------------------------
-     * PSEUDO CODE: 
+     * PSEUDO CODE:
      * 1. If the matrix A has no columns, the current partial solution is a valid
      * solution; terminate successfully
      * 1. Otherwise choose a column c (deterministically)
@@ -29,58 +24,63 @@ public class AlgorithmX {
      * 5. repeat this algorithm recursively on the reduced matrix A
      * source: https://en.wikipedia.org/wiki/Knuth%27s_Algorithm_X
      * 
-     * Knuth's algorithm uses the dancing link technique: 
-     * We tansform the exact cover problem in form of matrix of 0 and 1
-     * Each 1 is represented by a node of linked list and the whole matrix is transformed into a mesh of 4 way connected nodes
-     * Each node contains: 
+     * Knuth's algorithm uses the dancing link technique:
+     * We transform the exact cover problem in form of matrix of 0 and 1
+     * Each 1 is represented by a node of linked list and the whole matrix is
+     * transformed into a mesh of 4 way connected nodes
+     * Each node contains:
      ** Pointer to node left to it
      ** Pointer to node right of it
      ** Pointer to node above it
      ** Pointer to node below it
      ** Pointer to list header node to which it belongs
-
-     -------------------------------------------------------------------------------------------------
-
-} 
-     * source: 1. https://www.geeksforgeeks.org/exact-cover-problem-algorithm-x-set-2-implementation-dlx/?ref=lbp
-     *         2. https://www.geeksforgeeks.org/exact-cover-problem-algorithm-x-set-1/
+     * 
+     * -------------------------------------------------------------------------------------------------
+     * 
+     * }
+     * source: 1.
+     * https://www.geeksforgeeks.org/exact-cover-problem-algorithm-x-set-2-implementation-dlx/?ref=lbp
+     * 2. https://www.geeksforgeeks.org/exact-cover-problem-algorithm-x-set-1/
      * 
      * Genetisch algorithm:
-     * random character strings maken en aan een hele hoop data containers toevoegen 
+     * random character strings maken en aan een hele hoop data containers toevoegen
      * strings staan voor pentominoe peices die we willen gebruiken
      * voor iedere char string evaluate hoe goed die de ruimte vult en wat score is
      * met selectiemodel genen mixen en nieuwe generaties maken
      * 5% kans voor character om te veranderen in ander character
-     * Als heel vaak doen --> gemiddelde score voor elke generatie wordt beter (survival of fittest)
+     * Als heel vaak doen --> gemiddelde score voor elke generatie wordt beter
+     * (survival of fittest)
      * Heel dichtbij de oplossing
      * 
      * -----------------------------------------------------------------------------------------------
      * 
-     * REFERENCES: 
+     * REFERENCES:
      * https://www.ocf.berkeley.edu/~jchu/publicportal/sudoku/sudoku.paper.html
      * https://arxiv.org/pdf/cs/0011047.pdf
+     * 
      * @param args
      */
 
     public static void main(String[] args) {
-
+        // test with 4x4 and 5x5
     }
 
     public static int chooseColRow() {
         // TODO: Otherwise choose a column c (deterministically)
         // choose the column with the smallest possible size
-         // According to Donald Knuth's paper, it is most efficient to choose the column with the smallest possible size.
-   // That is what we do.
-        ColumnNode rightOfRoot = (ColumnNode)root.right; // we cast the node to the right of the root to be a ColumnNode
-        ColumnNode smallest = rightOfRoot; 
-        while(rightOfRoot.right != root) 
-        {
-         rightOfRoot = (ColumnNode)rightOfRoot.right;
-         if(rightOfRoot.size < smallest.size) // choosing which column has the lowest size
+        // According to Donald Knuth's paper, it is most efficient to choose the column
+        // with the smallest possible size.
+        // That is what we do.
+        ColumnNode rightOfRoot = (ColumnNode) root.right; // we cast the node to the right of the root to be a
+                                                          // ColumnNode
+        ColumnNode smallest = rightOfRoot;
+        while (rightOfRoot.right != root) {
+            rightOfRoot = (ColumnNode) rightOfRoot.right;
+            if (rightOfRoot.size < smallest.size) // choosing which column has the lowest size
             {
-            smallest = rightOfRoot;
-            }         
-        }      
+                smallest = rightOfRoot;
+            }
+        }
         return smallest;
     }
 
@@ -88,58 +88,53 @@ public class AlgorithmX {
         // TODO: remove the columns head by remapping the node to its left to the node
         // to its right so that the linked list no longer contains a way to access the
         // column head.
-        Node column = dataNode.column; 
+        Node column = dataNode.column;
 
-        column.right.left = column.left; 
-        column.left.right = column.right; 
+        column.right.left = column.left;
+        column.left.right = column.right;
 
-        for( Node row = column.down ; row != column ; row = row.down ) 
-            for( Node rightNode = row.right ; rightNode != row ; 
-                rightNode = rightNode.right ) { 
-                rightNode.up.down = rightNode.down; 
-                rightNode.down.up = rightNode.up; 
-            } 
-} 
-    
+        for (Node row = column.down; row != column; row = row.down)
+            for (Node rightNode = row.right; rightNode != row; rightNode = rightNode.right) {
+                rightNode.up.down = rightNode.down;
+                rightNode.down.up = rightNode.up;
+            }
+    }
+
     public void uncover() { // add back all values of the column of the list
-        //TODO: uncover column
-        Node column = dataNode.column; 
+        // TODO: uncover column
+        Node column = dataNode.column;
 
-        for( Node row = column.up ; row != column ; row = row.up ) 
-        for( Node leftNode = row.left ; leftNode != row ;
-         leftNode = leftNode.right ) { 
-         leftNode.up.down = leftNode; 
-         leftNode.down.up = leftNode; 
-        } 
-        column.right.left = column; 
-        column.left.right = column; 
-} 
-    
+        for (Node row = column.up; row != column; row = row.up)
+            for (Node leftNode = row.left; leftNode != row; leftNode = leftNode.right) {
+                leftNode.up.down = leftNode;
+                leftNode.down.up = leftNode;
+            }
+        column.right.left = column;
+        column.left.right = column;
+    }
+
     public void search(int k) {
-        if( h.right == h ) { 
+        if (h.right == h) {
             System.out.println(partialSolution);
-        return; 
-       } else { 
-        ColumnNode column = chooseColumn(); 
-        cover(column); 
-    
-        for( Node row = column.down ; rowNode != column ;
-           rowNode = rowNode.down ) { 
-               partialSolution.add( rowNode ); 
-    
-               for( Node rightNode = row.right ; rightNode != row ;
-                    rightNode = rightNode.right ) 
-                       cover( rightNode ); 
-    
-        search( k+1); 
-        partialSolution.remove( rowNode ); 
-        column = rowNode.column; 
-    
-        for( Node leftNode = rowNode.left ; leftNode != row ;
-                 leftNode = leftNode.left )                                                                             
-               uncover( leftNode ); 
-        } 
-        uncover( column );    
+            return;
+        } else {
+            ColumnNode column = chooseColumn();
+            cover(column);
+
+            for (Node row = column.down; rowNode != column; rowNode = rowNode.down) {
+                partialSolution.add(rowNode);
+
+                for (Node rightNode = row.right; rightNode != row; rightNode = rightNode.right)
+                    cover(rightNode);
+
+                search(k + 1);
+                partialSolution.remove(rowNode);
+                column = rowNode.column;
+
+                for (Node leftNode = rowNode.left; leftNode != row; leftNode = leftNode.left)
+                    uncover(leftNode);
+            }
+            uncover(column);
         }
     }
 }
